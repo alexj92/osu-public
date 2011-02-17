61f18c539dfad05e43c025c57e293e28ed3375fb
alexj92 <hahanottelling@gmail.com>
2011-02-17 04:26:04 -0800

    Remove underscores, fix defines

--- mirror/config.php
+++ mirror/config.php
@@ -1,9 +1,9 @@
 <?
 	//configuration constants
-	define(SECRET_KEY,'mirror_key_goes_here');
-	define(FILES_DIRECTORY,'mapsgohere/');
-	define(MAX_LINK_AGE,120);
-	define(URL_FOPEN_SUPPORT,true);
-	define(USE_XSENDFILE,false);
-        define(USE_X_ACCEL_REDIRECT,false);
+	define('SECRET_KEY','mirror_key_goes_here');
+	define('FILES_DIRECTORY','mapsgohere/');
+	define('MAX_LINK_AGE',120);
+	define('URL_FOPEN_SUPPORT',true);
+	define('USE_XSENDFILE',false);
+        define('USE_XACCELREDIRECT',false);
 ?>
